f03f232f15f68224d8ce43ab90c6c514777930ff
Aivar Kripsaar <aivarkripsaar@gmail.com>
2018-07-10 15:48:32 +0200

    Adds a fetching of song list before library has been refreshed

--- Server/MusicScream/MusicScream/ClientApp/components/MusicPlayer/MusicPlayer.tsx
+++ Server/MusicScream/MusicScream/ClientApp/components/MusicPlayer/MusicPlayer.tsx
@@ -37,6 +37,7 @@ export class MusicPlayer extends React.Component<{}, IMusicPlayerState>
 
     public componentDidMount()
     {
+        this.getSongList();
         this.refreshLibrary();
     }
 
