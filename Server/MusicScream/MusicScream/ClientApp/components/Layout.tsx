70cd044ea08760bff41343fc35f0dc1c22b5ba59
Aivar Kripsaar <aivarkripsaar@gmail.com>
2018-10-07 10:39:43 +0200

    Improves page layout

--- Server/MusicScream/MusicScream/ClientApp/components/Layout.tsx
+++ Server/MusicScream/MusicScream/ClientApp/components/Layout.tsx
@@ -11,15 +11,15 @@ export class Layout extends React.Component<LayoutProps, {}> {
 
     public render() {
         return <div className='container-fluid'>
-            <div className='row'>
+            <div className='row' style={{height: "calc(100vh - 80px)"}}>
                 <div className='col-sm-3'>
                     <NavMenu />
                 </div>
-                <div className='col-sm-9'>
+                <div className='col-sm-9' style={{height: "100%", overflowY: "auto"}}>
                     { this.props.children }
                 </div>
             </div>
-            <div style={{position: "fixed", bottom: 0, left: 0, width: "100%", height: "80px", zIndex: 2}}>
+            <div style={{position: "relative", bottom: 0, left: "-15px", width: "100vw", height: "80px", zIndex: 2}}>
                 <MusicPlayerControls/>
             </div>
         </div>;
