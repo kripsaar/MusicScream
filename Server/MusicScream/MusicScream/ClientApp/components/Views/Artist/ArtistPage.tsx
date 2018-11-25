dca8f0887e8c812e50225b8d4775c1152e11703b
Aivar Kripsaar <aivarkripsaar@gmail.com>
2018-11-25 17:44:47 +0100

    Adds more info to artist page

--- Server/MusicScream/MusicScream/ClientApp/components/Views/Artist/ArtistPage.tsx
+++ Server/MusicScream/MusicScream/ClientApp/components/Views/Artist/ArtistPage.tsx
@@ -1,4 +1,8 @@
 import * as React from 'react';
+import { Communication } from '../../../Communication';
+import { Artist } from '../../../Models/ArtistModel';
+import { Link } from 'react-router-dom';
+import { MdAlbum } from 'react-icons/md';
 
 interface IArtistPageProps
 {
@@ -7,7 +11,7 @@ interface IArtistPageProps
 
 interface IArtistPageState
 {
-
+    artist: Artist;
 }
 
 export class ArtistPage extends React.Component<IArtistPageProps, IArtistPageState>
@@ -15,12 +19,84 @@ export class ArtistPage extends React.Component<IArtistPageProps, IArtistPageSta
     constructor(props: IArtistPageProps, state: IArtistPageState)
     {
         super(props, state);
+        this.state = {artist: {id: 0, name: "", aliases: [], albums: [], songs: [], units: [], unitMembers: []}};
+    }
+
+    public componentDidMount()
+    {
+        this.getArtist(this.props.id);
+    }
+
+    public componentWillReceiveProps(nextProps: IArtistPageProps)
+    {
+        if (nextProps.id != this.state.artist.id)
+            this.getArtist(nextProps.id);
+    }
+
+    private async getArtist(id: number)
+    {
+        let data = await Communication.simpleAjaxPromise("Music/GetArtist?Id=" + id);
+        if (data.artist)
+        {
+            var artist = data.artist
+            this.setState({artist: artist});
+        }
     }
 
     public render()
     {
+        var artist = this.state.artist;
+        var name = this.state.artist.name;
         return <div>
-                {"Artist ID: " + this.props.id}
+                <div style={{fontSize: "3em"}}>{name}</div>
+                <div>
+                    <span style={{fontWeight: "bold"}}>{"Also known as: "}</span>
+                    <span>{artist.aliases.join(", ")}</span>
+                </div>
+                { artist.units.length > 0 ?
+                    <div>
+                        <span style={{fontWeight: "bold"}}>Member of: </span>
+                        {artist.units.map((value, index) =>
+                            <span>
+                                <span>{(index == 0 ? "" : ", ")}</span>
+                                <Link to={"/Artist/" + value.id}>
+                                    {value.name}
+                                </Link>
+                            </span>
+                        )}
+                    </div>
+                : null }
+                { artist.unitMembers.length > 0 ?
+                    <div>
+                        <span style={{fontWeight: "bold"}}>Members: </span>
+                        {artist.unitMembers.map((value, index) =>
+                            <span>
+                                <span>{(index == 0 ? "" : ", ")}</span>
+                                <Link to={"/Artist/" + value.id}>
+                                    {value.name}
+                                </Link>
+                            </span>
+                        )}
+                    </div>
+                : null }
+                <br/>
+                <span style={{fontWeight: "bold"}}>Albums: </span>
+                <ul className="selection-list" style={{display: "flex", flexDirection: "column"}}>
+                    { artist.albums.map((element, index) =>
+                        <li key={"album-" + element.id} className="list-item">
+                            {element.title}
+                        </li>
+                    )}
+                </ul>
+                <br/>
+                <span style={{fontWeight: "bold"}}>Songs: </span>
+                <ul className="selection-list" style={{display: "flex", flexDirection: "column"}}>
+                    { artist.songs.map((element, index) =>
+                        <li key={"album-" + element.id} className="list-item">
+                            {element.title}
+                        </li>
+                    )}
+                </ul>
             </div>
     }
 }
