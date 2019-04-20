a41c5ad59a1ed0951def1a6a3acba59e74beeb30
Aivar Kripsaar <aivarkripsaar@gmail.com>
2019-04-20 14:45:36 +0200

    Minor changes

---
+++ Server/MusicScream/MusicScream/Utilities/PlaylistHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MusicScream.Models;
+
+namespace MusicScream.Utilities
+{
+    public class PlaylistHandler
+    {
+        private readonly MusicScreamContext _dbContext;
+
+        public PlaylistHandler(MusicScreamContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
